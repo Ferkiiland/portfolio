789b74ffee3248f5d9d6be39cbe7536927fe3845
Ferkiiland <113061637+Ferkiiland@users.noreply.github.com>
2024-09-08 06:28:31 -0500

    src uploaded

---
+++ src/js/swiper.js
@@ -0,0 +1,32 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    spaceBetween: 1,
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: false,
+      dynamicBullets: true,
+      dynamicMainBullets: 2,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      enabled: false,
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      enabled: false,
+      el: '.swiper-scrollbar',
+    },
+    autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true,
+    },
+  });
